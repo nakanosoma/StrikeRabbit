c00183d54ff903af4614cec0db4d68116e322435
中野宗馬 <snowbell0704@gmail.com>
2016-07-01 14:41:09 +0900

    test
    test

---
+++ TryJOGL_Programming_Process/tryjogl/Example.java
@@ -0,0 +1,21 @@
+package tryjogl;
+
+/**
+ * JOGL（Java bindings for OpenGL）の例題プログラムである。<br>
+ * OpenGLを使ったJavaによる三次元グラフィックスの初歩的な練習になる。<br>
+ */
+public class Example extends Object
+{
+	/**
+	 * 針金細工のティーポット（Wire Teapot）を描くOpenGLのウィンドウを開く。<br>
+	 * @param arguments コマンドの引数列（文字列の配列）<br>
+	 */
+	public static void main(String[] arguments)
+	{
+		System.out.println("main");
+
+		// バージョンを追いながらプログラムを作ってゆきましょう。
+
+		return;
+	}
+}
